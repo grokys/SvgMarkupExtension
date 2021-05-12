185f547fd3636ca1344f2c2d2fed0683de06b64a
Steven Kirk <grokys@gmail.com>
2021-05-12 18:18:58 +0200

    Add project files.

---
+++ SvgMarkupExtension/SvgImageExtension.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Avalonia;
+using Avalonia.Markup.Xaml;
+using Avalonia.Platform;
+using Avalonia.Svg.Skia;
+
+namespace SvgMarkupExtension
+{
+    public class SvgImageExtension : MarkupExtension
+    {
+        public SvgImageExtension(string name) => Name = name;
+
+        public string Name { get; }
+
+        public override object ProvideValue(IServiceProvider serviceProvider)
+        {
+            var uri = (IUriContext)serviceProvider.GetService(typeof(IUriContext))!;
+            var loader = AvaloniaLocator.Current.GetService<IAssetLoader>();
+            var stream = loader.Open(new Uri(Name, UriKind.Relative), uri.BaseUri);
+            var source = new SvgSource();
+            source.Load(stream);
+            return new SvgImage { Source = source };
+        }
+    }
+}
